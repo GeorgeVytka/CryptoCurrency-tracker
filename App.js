3fcb186fbe0752f1aacade2453280d2ccef2bee9
George Vytka <george.vytka@yahoo.com>
2021-08-04 14:10:09 -0400

    added list items

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Listitem from "./components/Listitem";
 
 export default function App() {
   return (
@@ -10,6 +11,11 @@ export default function App() {
       </View>
 
       <View style={styles.divder}></View>
+
+      {/* List the crypto */}
+      <View>
+        <Listitem />
+      </View>
     </View>
   );
 }
--- components/Listitem.js
+++ components/Listitem.js
@@ -1,12 +1,67 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+//mport eth from "../assets/icon.png";
 
-const ListItem = () => {
+const ListItem = ({
+  name,
+  symbol,
+  currentPrice,
+  priceChangePercentage7d,
+  logoUrl,
+}) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <TouchableOpacity>
+      <View style={styles.itemWrapper}>
+        {/** This is the Lft side */}
+
+        <View style={styles.leftSide}>
+          <Image
+            source={require("../assets/image/eth.png")}
+            style={styles.image}
+          />
+          {/*TODO: make dynamic */}
+          <View style={styles.tittleWrapper}>
+            <Text style={styles.title}>Ethereum</Text>
+            <Text style={styles.subtitle}>ETH</Text>
+          </View>
+        </View>
+
+        {/** This is the Right side */}
+        <View style={styles.rightSide}>
+          <Text style={styles.title}>Ethereum</Text>
+          <Text style={(styles.subtitle, { color: "red" })}>ETH</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  itemWrapper: {
+    paddingHorizontal: 17,
+    marginTop: 24,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  leftSide: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    height: 48,
+    width: 48,
+  },
+  tittleWrapper: { marginLeft: 7 },
+  title: { fontSize: 19 },
+  subtitle: {
+    marginTop: 5,
+    fontSize: 14,
+    color: "gray",
+  },
+  rightSide: {
+    alignItems: "flex-end",
+  },
+});
 
 export default ListItem;
